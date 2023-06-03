a6b188a3bcb89b072775470298024f0292afd711
”Kodoku” <kodokunawa@gmail.com>
2023-06-03 18:14:23 +0300

    css анимация заменена на js анимацию

---
+++ main.js
@@ -0,0 +1,39 @@
+(() => {
+    const way = $('.lead__svg-way');
+    const waypoints = $('.lead__svg-waypoint');
+    const towns = $('.lead__svg-city');
+    const breakpoints = [
+        {point: 1390, element: [towns[0], waypoints[0], waypoints[1]], isVisible: false},
+        {point: 1529, element: [towns[1], waypoints[2], waypoints[3]], isVisible: false},
+        {point: 1702, element: [towns[2], waypoints[4], waypoints[5]], isVisible: false},
+        {point: 1771, element: [towns[3], waypoints[6], waypoints[7]], isVisible: false},
+        {point: 1904, element: [towns[4], waypoints[8], waypoints[9]], isVisible: false},
+        {point: 1994, element: [towns[5], waypoints[10], waypoints[11]], isVisible: false},
+        {point: 2224, element: [towns[6], waypoints[12], waypoints[13]], isVisible: false},
+        {point: 2293.5, element: [towns[7], waypoints[14], waypoints[15]], isVisible: false},
+        {point: 2363, element: [towns[8], waypoints[16], waypoints[17]], isVisible: false},
+        {point: 2502, element: [towns[9], waypoints[18], waypoints[19]], isVisible: false},
+        {point: 2689.5, element: [towns[10], waypoints[20], waypoints[21]], isVisible: false},
+        {point: 2760, element: [towns[11], waypoints[22], waypoints[23]], isVisible: false}
+    ]
+
+    way.animate({
+        strokeDashoffset: 2780
+    },
+    {
+        progress: () => animatePoints(breakpoints, $('.lead__svg-way').css('stroke-dashoffset').slice(0, 4)),
+        duration: 20000,
+        easing: 'linear'
+    });
+
+    function animatePoints(breakpoints, lineLength){
+        breakpoints.forEach(item => {
+            if(item.isVisible) return;
+
+            if(item.point <= lineLength){
+                item.element.forEach(item => $(item).animate({opacity: 1}, 500, 'linear'))
+                item.isVisible = true;
+            }
+        });
+    }
+})();
